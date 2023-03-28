826906c0c0ef31d686672298f831ecd120856118
mburwell <mburwell@iu.edu>
2023-03-28 00:01:48 -0400

    Don't know what I did here

--- src/ThreadNode.h
+++ src/ThreadNode.h
@@ -10,16 +10,25 @@
 
 #define MAX 1024
 
-// struct Message
-// {
-// 	/* data */
-// 	uint16_t trsmtr;
-// 	uint16_t rcvr;
-// 	uint16_t sendr;
-// 	uint16_t dest;
-// 	uint16_t hCount;
-// };
+/*
+	// Create graph from data file
+	ThreadGraph graph("graph/A10.dat");
 
+	std::cout << "Nodes: " << graph.getNumNodes() << std::endl;
+	std::cout << "Edges: " << graph.getNumEdges() << std::endl;
+
+	std::vector<uint16_t>::iterator it;
+
+	// get the neighbors/edges of node zero "0"
+	std::vector<uint16_t> temp = graph.getNeighbors(0);
+
+	// print node zero's neighbors/edges
+	std::cout << "0 --> ";
+	for(it = temp.begin(); it != temp.end(); it++){
+		std:: cout << *it << "-";
+	}
+	std::cout << std::endl;
+*/
 class ThreadNode
 {
 	public:
