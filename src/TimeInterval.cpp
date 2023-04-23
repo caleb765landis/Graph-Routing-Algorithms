3bca2aef7b98b6e1e5d7b953a14b286da2829e48
mburwell <mburwell@iu.edu>
2023-04-23 06:23:59 -0400

    Made some additions to timeInterval

--- src/TimeInterval.cpp
+++ src/TimeInterval.cpp
@@ -12,12 +12,34 @@ void TimeInterval::stop()
         gettimeofday(&this->end_time, NULL);
 }
 
+void TimeInterval::chronoStart()
+{
+        _start_time = high_resolution_clock::now();
+}
+
+void TimeInterval::chronoStop()
+{
+        _end_time = high_resolution_clock::now();
+}
+
 double TimeInterval::GetInterval()
 {
         double t =(double)(end_time.tv_sec-start_time.tv_sec)*1000000.0+(double)(end_time.tv_usec-start_time.tv_usec);  // in micro-sec
         // t = t / 1000000.0               // in sec
 
         return t;
+}
+
+double TimeInterval::getChronoInterval()
+{
+        duration<double> t = _end_time - _start_time;
+
+        return t.count();
+}
+
+time_point<high_resolution_clock> TimeInterval::projectedEnd(int seconds) const
+{
+        return this->_start_time + duration<int>(seconds);
 }
 
 void TimeInterval::setTimes(double st, double ft)
--- src/TimeInterval.h
+++ src/TimeInterval.h
@@ -2,21 +2,31 @@
 #define _TIMEINTREVAL_H_
 
 #include <sys/time.h>
+#include <chrono>
 #include <cstddef>
+
+using namespace std::chrono;
 
 class TimeInterval{
 public:
+        time_point<high_resolution_clock> _start_time;
+        time_point<high_resolution_clock> _end_time;
+
         timeval start_time;
         timeval end_time;
 
         TimeInterval();
         void start();
+        void chronoStart();
         void stop();
+        void chronoStop();
 
         void setTimes(double st, double ft);
         timeval getStartTime() const;
         timeval getEndTime() const;
         double GetInterval();
+        double getChronoInterval();
+        time_point<high_resolution_clock> projectedEnd(int seconds) const;
 
         TimeInterval operator=(const TimeInterval& other);
 };
